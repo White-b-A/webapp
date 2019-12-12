2adb928fa1f38880e75a11ba9ec63a7601a62e12
Light <[ghost1928ps@icloud.com]>
2019-12-12 18:17:20 +0900

    変更

--- src/webapp/Webapp2.java
+++ src/webapp/Webapp2.java
@@ -1,9 +1,11 @@
-package ooptest2;
+package webapp;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,8 +18,8 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ooptest2
  */
-@WebServlet("/ooptest2")
-public class ooptest2 extends HttpServlet {
+@WebServlet("/Webapp2")
+public class Webapp2 extends HttpServlet {
 	private String encoding = "UTF-8";;
 	String sql = "select * from user where id = ? and password = ?" ;
 	/**
@@ -28,28 +30,11 @@ public class ooptest2 extends HttpServlet {
 		response.setContentType("text/html; charset="+encoding);
 		String id = request.getParameter("userid");
 		String pass = request.getParameter("password");
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-				Connection con = DriverManager.getConnection("jdbc://172.20.10.3/test?serverTimezone="
-						+ "JST","root","root");
-
-//				PreparedStatement stmt = con.prepareStatement(sql);
-//				stmt.setString(1,id);
-//				stmt.setString(2,pass);
-//				ResultSet rs = stmt.executeQuery();
-
-			}catch(Exception ex) {
-				String msg = "ドライバのロードに失敗しました";
-			}
-
-
 		String sql = "select * from user";
 		PrintWriter out = response.getWriter();
-
 		out.println("<!DOCTYPE html>");
 		out.println("<html>");
 		out.println("<head>");
-		//out.println("<meta charset=\"UTF-8\">");
 		out.println("<link rel=\"stylesheet\" href=\"ooptest.css\">");
 		out.println("<title>メインメニュー</title>");
 		out.println("</head>");
@@ -61,5 +46,24 @@ public class ooptest2 extends HttpServlet {
 		out.println("</body>");
 		out.println("</html>");
 	}
+	protected boolean user(String id,String pass) {
+		if (user == null || user.length() == 0 || pass == null || pass.length() == 0){
+		      return false;
+		    }
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection con = DriverManager.getConnection("jdbc://172.20.10.3/test?serverTimezone="
+						+ "JST","root","root");
+
+				PreparedStatement stmt = con.prepareStatement(sql);
+				stmt.setString(1,id);
+				stmt.setString(2,pass);
+				ResultSet rs = stmt.executeQuery();
+
+			}catch(Exception ex) {
+				String msg = "ドライバのロードに失敗しました";
+			}
+	}
+
 
 }
