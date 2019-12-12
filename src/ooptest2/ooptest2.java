5d02644d8f18e0d0452602b4c49a8e5b71517139
Light <[ghost1928ps@icloud.com]>
2019-12-12 14:50:23 +0900

    途中

--- src/ooptest2/ooptest2.java
+++ src/ooptest2/ooptest2.java
@@ -2,6 +2,8 @@ package ooptest2;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,24 +19,33 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ooptest2")
 public class ooptest2 extends HttpServlet {
 	private String encoding = "UTF-8";;
-
+	String sql = "select * from user where id = ? and password = ?" ;
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding(encoding);
 		response.setContentType("text/html; charset="+encoding);
-//		try {
-//			Class.forName("com.mysql.cj.jdbc.Driver");
-//				Connection con = DriverManager.getConnection("jdbc://172.20.10.3/test?serverTimezone="
-//						+ "JST","root","root");
-//			}
-//		}
-
 		String id = request.getParameter("userid");
 		String pass = request.getParameter("password");
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection con = DriverManager.getConnection("jdbc://172.20.10.3/test?serverTimezone="
+						+ "JST","root","root");
+
+//				PreparedStatement stmt = con.prepareStatement(sql);
+//				stmt.setString(1,id);
+//				stmt.setString(2,pass);
+//				ResultSet rs = stmt.executeQuery();
+
+			}catch(Exception ex) {
+				String msg = "ドライバのロードに失敗しました";
+			}
+
+
 		String sql = "select * from user";
 		PrintWriter out = response.getWriter();
+
 		out.println("<!DOCTYPE html>");
 		out.println("<html>");
 		out.println("<head>");
